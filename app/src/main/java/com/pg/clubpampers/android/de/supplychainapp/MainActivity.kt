e16ef23bef78718d263bb5873061ce2041c5e399
Babar Shamsi <babar@aymcommerce.com>
2021-11-24 18:26:59 +0500

    Initial Commit

---
+++ app/src/main/java/com/pg/clubpampers/android/de/supplychainapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.pg.clubpampers.android.de.supplychainapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
